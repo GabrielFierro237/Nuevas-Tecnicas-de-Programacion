7be03c2e66a4921f887e51257cc0622c7d218498
GabrielFierro237 <gabrielandro12121212@gmail.com>
2021-06-23 19:28:56 -0500

    Deber

--- NTP/Modulo_1/basic/10_Clases.js
+++ NTP/Modulo_1/basic/10_Clases.js
@@ -1,34 +1,33 @@
 //como se hacia antes
 
 function Rectangulo(base, altura) {
-    this.base = base;
-    this.altura = altura;
-};
+  this.base = base;
+  this.altura = altura;
+}
 
-Rectangulo.prototype.calculoArea = function() {
-    return this.base * this.altura;
+Rectangulo.prototype.calculoArea = function () {
+  return this.base * this.altura;
 };
 
 var rectangulo = new Rectangulo(6, 3);
 
-console.log(rectangulo.calculoArea());
-console.log(rectangulo.base);
+//console.log(rectangulo.calculoArea());
+//console.log(rectangulo.base);
 
 //declaracion de clases
-class Rectangulo2{
-    constructor(base, altura){
-        this.base = base;
-        this.altura = altura;
-    }
+class Rectangulo2 {
+  constructor(base, altura) {
+    this.base = base;
+    this.altura = altura;
+  }
 
-    calculoArea(){
-        return this.base * this.altura;
-    }
-};
+  calculoArea() {
+    return this.base * this.altura;
+  }
+}
 
 var rectangulo2 = new Rectangulo2(6, 4);
-console.log(rectangulo2.calculoArea());
-
+//console.log(rectangulo2.calculoArea());
 
 //clase persona
 // implementar getter, setter y realizzar una implementacion
@@ -38,3 +37,75 @@ console.log(rectangulo2.calculoArea());
 /*
     DEBER
 */
+/*
+    Get y set
+*/
+let persona = {
+  nombre: "Alejandro",
+  apellido: "Mora",
+  get nombreCompleto() {
+    return `${nombre} ${apellido}`;
+  },
+  set nombreCompleto(nom) {
+    const palabras = nom.split(" ");
+    this.nombre = palabras[0] || "";
+    this.apellido = palabras[1] || "";
+  },
+};
+
+persona.nombreCompleto = "Gabriel Fierro";
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+
+/*
+    metodos estaticos
+*/
+class Punto {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distancia(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+}
+
+const p1 = new Punto(40, 89);
+const p2 = new Punto(59, 14);
+
+console.log(Punto.distancia(p1, p2));
+
+/*
+Construir Clase Persona y estudiante, extender Estudiante con Persona
+*/
+class Persona {
+  constructor(person) {
+    this.nombreper = person;
+  }
+  presentacion() {
+    return "Me llamo " + this.nombreper + " Soy una Persona comun";
+  }
+}
+
+class Estudiante extends Persona {
+  constructor(person, estud) {
+    super(person);
+    this.nombreest = estud;
+  }
+  show() {
+    return (
+      this.presentacion() +
+      ", yo me llamo " +
+      this.nombreest +
+      " Soy un estudiante comun"
+    );
+  }
+}
+
+estper = new Estudiante("Gabriel", "Alejandro");
+console.log(estper.show());
